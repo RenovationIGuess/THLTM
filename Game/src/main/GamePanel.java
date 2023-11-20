f037a4417773a6f14f964e8ad336e5d96c714957
Ramy <nguyentunglam724@gmail.com>
2023-11-20 22:36:13 +0700

    feat: add game's player's movement and basic tiles

---
+++ Game/src/main/GamePanel.java
@@ -0,0 +1,131 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+import entity.Player;
+import tiles.TileManager;
+
+public class GamePanel extends JPanel implements Runnable {
+	private static final long serialVersionUID = 1L;
+	// SCREEN SETTINGS
+	// Default size of characters, NPCs, tiles
+	final int originalTileSize = 16; // 16x16 tile
+	final int scale = 3;
+	
+	public final int tileSize = originalTileSize * scale; // 48x48 tile
+	// Ratio 4:3
+	public final int maxScreenCol = 16;
+	public final int maxScreenRow = 12;
+	public final int screenWidth = tileSize * maxScreenCol;
+	public final int screenHeight = tileSize * maxScreenRow;
+	
+//	FPS
+	int FPS = 144;
+	
+	TileManager tileM = new TileManager(this);
+	KeyHandler keyH = new KeyHandler();
+	Thread gameThread;
+	Player player = new Player(this, keyH);
+	
+	// Set player's default position
+//	int playerX = 100;
+//	int playerY = 100;
+//	int playerSpeed = 4;
+	
+	public GamePanel() {
+		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+		this.setBackground(Color.black);
+		this.setDoubleBuffered(true);
+		this.addKeyListener(keyH);
+		this.setFocusable(true);
+	}
+	
+	public void startGameThread() {
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+	
+	// @Override
+	public void run() {
+//		Sleep method
+//		double drawInterval = 1000000000/FPS;
+//		double nextDrawTime = System.nanoTime() + drawInterval;
+//		
+//		while (gameThread != null) {
+//			// long currentTime = System.nanoTime(); // More precise
+//			// long currentTime2 = System.currentTimeMillis();
+//			
+//			// 1. UPDATE: update information such as character positions
+//			update();
+//			
+//			// 2. DRAW: draw the screen with the updated information
+//			repaint(); // This is how to call the paintComponent method
+//			
+//			try {
+//				double remainingTime = nextDrawTime - System.nanoTime();
+//				remainingTime = remainingTime/1000000;
+//				
+//				if (remainingTime < 0) {
+//					remainingTime = 0;
+//				}
+//				
+//				Thread.sleep((long) remainingTime);
+//				
+//				nextDrawTime += drawInterval;
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		}
+		
+//		Delta / Accumulator method
+		double drawInterval = 1000000000/FPS;
+		double delta = 0;
+		long lastTime = System.nanoTime();
+		long currentTime;
+		long timer = 0;
+		int drawCount = 0;
+		
+		while (gameThread != null) {
+			currentTime = System.nanoTime();
+			
+			delta += (currentTime - lastTime) / drawInterval;
+			timer += (currentTime - lastTime);
+			lastTime = currentTime;
+			
+			if (delta >= 1) {
+				update();
+				repaint();
+				delta--;
+				drawCount++;
+			}
+			
+			if (timer > 1000000000) {
+				System.out.println("FPS: " + drawCount);
+				drawCount = 0;
+				timer = 0;
+			}
+		}
+	}
+	
+	public void update() {
+		player.update();
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		Graphics2D g2 = (Graphics2D)g;
+		
+		tileM.draw(g2);
+		
+		player.draw(g2);
+		
+		g2.dispose();
+	}
+}
