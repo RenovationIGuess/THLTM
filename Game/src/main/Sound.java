2d0455606c0e0e1b3e78a3f5bbe7214b4b788fea
Ramy <nguyentunglam724@gmail.com>
2023-12-12 23:15:07 +0700

    feat: add video #25

--- Game/src/main/Sound.java
+++ Game/src/main/Sound.java
@@ -16,6 +16,11 @@ public class Sound {
 		soundURL[2] = getClass().getResource("/sound/powerup.wav");
 		soundURL[3] = getClass().getResource("/sound/fanfare.wav");
 		soundURL[4] = getClass().getResource("/sound/unlock.wav");
+		soundURL[5] = getClass().getResource("/sound/hitmonster.wav");
+		soundURL[6] = getClass().getResource("/sound/receivedamage.wav");
+		soundURL[7] = getClass().getResource("/sound/swingweapon.wav");
+		
+		soundURL[20] = getClass().getResource("/sound/fontaine.wav");
 	}
 	
 	public void setFile(int i) {
@@ -24,7 +29,7 @@ public class Sound {
 			clip = AudioSystem.getClip();
 			clip.open(ais);
 		} catch (Exception e) {
-			
+			e.printStackTrace();
 		}
 	}
 	
